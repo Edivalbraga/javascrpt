47183304259d6b9b09f5a5850208e529e92f8805
Edival Braga Filho <edivalbragafilho@gmail.com>
2023-10-09 10:49:02 -0300

    Funcionalidade aos botões operadopres

--- calculadora001/aula70.js
+++ calculadora001/aula70.js
@@ -1,22 +1,65 @@
-const teclasNumericas=[...document.querySelectorAll(".num")]
-const teclasOp=[...document.querySelectorAll(".op")]
-const teclaRes=document.querySelector(".res")
-const display=document.querySelector(".display")
-const teclaOn=document.getElementById("ton")
-const limpar=document.getElementById("tlimpar")
+const teclasNumericas = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const teclaOn = document.getElementById("ton")
+const limpar = document.getElementById("tlimpar")
+const tigual= document.getElementById("tigual")
 
-teclasNumericas.forEach((el)=>{
-    el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+let sinal = false
+let decimal=false
+
+
+teclasNumericas.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal=false
+        
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+                decimal=true
+                if(display.innerHTML=="0"){
+                    display.innerHTML="0,"
+                }else{
+                    display.innerHTML += evt.target.innerHTML
+                }
+
+            }
+            
+        }else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            display.innerHTML += evt.target.innerHTML
+
+        }
+        
     })
 })
-teclasOp.forEach((el)=>{
-    el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+teclasOp.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (!sinal) {
+            sinal = true
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
     })
 })
-limpar.addEventListener("click",(evt)=>{
-    display.innerHTML="0"
+limpar.addEventListener("click", (evt) => {
+    sinal=false
+    decimal=false
+    display.innerHTML = "0"
+})
+tigual.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    const res =eval(display.innerHTML)
+    display.innerHTML=res
 })
 
 console.log(teclasNumericas)
